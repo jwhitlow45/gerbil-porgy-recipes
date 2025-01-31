f5864b57fa5be0bfc6e42013411ce4711b67a600
jwhitlow45 <jwhitlow45@gmail.com>
2025-01-31 14:49:11 -0800

    add fade in on page load

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ function App() {
   }, [path]);
 
   return (
-    <div>
+    <div id="app">
       <Header style={headerStyle} />
       <div id="body" style={bodyStyle}>
         {showHomePage && <PostList />}
